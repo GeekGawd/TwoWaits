7fa0ad53746bbf93e688570a0b7a8995db775eb6
Akshay-Bhatia22 <91560853+Akshay-Bhatia22@users.noreply.github.com>
2021-12-23 11:33:18 +0530

    Hosting on Heroku

--- Project_TwoWaits/Project_TwoWaits/settings.py
+++ Project_TwoWaits/Project_TwoWaits/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 from django.utils.timezone import timedelta
 import os
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -91,6 +91,9 @@ WSGI_APPLICATION = 'Project_TwoWaits.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 DATABASES = {
     'default': {
